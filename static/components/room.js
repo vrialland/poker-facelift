89fbe7f72a2a0df2abb7e819fe7a46864a3b465a
Sylvain Delabye <sylvain.delabye@people-doc.com>
2021-02-12 16:01:22 +0100

    UI improvement: centered content

--- static/components/room.js
+++ static/components/room.js
@@ -2,35 +2,36 @@ Vue.component('room', {
   template: `
     <div>
       <h3>Welcome {{name}}</h3>
-      <label for="players">List of players:</label>
-      <ul id="players">
-        <li class="player" v-for="player in players">
-          <span v-if="player.name == name">
-            <strong>{{player.name}}</strong>
-            <span class="choice">&nbsp;| {{player.choice}}</span>
+      <div class="players">
+        <ul id="players">
+          <li class="player" v-for="player in players">
+            <span v-if="player.name == name">
+              <strong>{{player.name}}</strong>
+              <span class="choice">&nbsp;| {{player.choice}}</span>
+            </span>
+            <span v-else>
+              {{player.name}}
+              <span class="choice">&nbsp;| <span v-bind:class="{hidden: hideChoices}">{{player.choice}}</span></span>
+            </span>
+          </li>
+          <span id="observers">
+            <li v-for="observer in observers">
+              <span v-if="observer.name == name">
+                <strong>{{observer.name}}</strong>
+              </span>
+              <span v-else>
+                {{observer.name}}
+              </span>
+            </li>
           </span>
-          <span v-else>
-            {{player.name}}
-            <span class="choice">&nbsp;| <span v-bind:class="{hidden: hideChoices}">{{player.choice}}</span></span>
-          </span>
-        </li>
-      </ul>
-      <label for="observers">List of observers:</label>
-      <ul id="observers">
-        <li v-for="observer in observers">
-          <span v-if="observer.name == name">
-            <strong>{{observer.name}}</strong>
-          </span>
-          <span v-else>
-            {{observer.name}}
-          </span>
-        </li>
-      </ul>
+        </ul>
+      </div>
+      <br>
       <vote v-bind:name="name"/>
       <br><br>
       <button v-on:click="reveal()">Reveal</button>
-      <button v-on:click="clear()">Clear</button>
-    </div>`,
+      <button v-on:click="clear()">Clear</button><br><br>
+          </div>`,
   computed: {
     hideChoices: function() {
       if (this.finished) {
